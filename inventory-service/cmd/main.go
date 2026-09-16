c38a458c8d0464d354abffafd0e6f0d5f8d3d9c3
souzannatha <souzanatha04@gmail.com>
2026-09-16 18:05:19 -0300

    feat: add conn.go and godotenv in main.go and conn.go

--- inventory-service/cmd/main.go
+++ inventory-service/cmd/main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	r := gin.Default()
+	server := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Não foi possível carregar o .env")
+	}
+
+	// dbConnection, err := db.ConnectDB()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	server.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
-	r.Run()
+	server.Run()
 }
