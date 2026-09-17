6940276a9ce3951a6f6f7db67085ad331dc7ef96
souzannatha <souzanatha04@gmail.com>
2026-09-17 14:23:54 -0300

    feat: export  ProductControlle,  add GetProductController and GetProductByCodeController

--- inventory-service/controller/product_controller.go
+++ inventory-service/controller/product_controller.go
@@ -8,22 +8,23 @@ import (
 	"github.com/souzannatha/Korp_Teste_Natha/inventory-service/usecase"
 )
 
-type productController struct {
+type ProductController struct {
 	productUseCase usecase.ProductUseCase
 }
 
-func NewProductController(usecase usecase.ProductUseCase) productController {
-	return productController{
+func NewProductController(usecase usecase.ProductUseCase) ProductController {
+	return ProductController{
 		productUseCase: usecase,
 	}
 }
 
-func (p *productController) CreateProductController(ctx *gin.Context) {
+func (p *ProductController) CreateProductController(ctx *gin.Context) {
 	var product model.Product
 	err := ctx.BindJSON(&product)
 
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, err)
+		return
 	}
 
 	insertedProduct, err := p.productUseCase.CreateProductUseCase(product)
@@ -32,4 +33,44 @@ func (p *productController) CreateProductController(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, insertedProduct)
+}
+
+func (p *ProductController) GetProductController(ctx *gin.Context) {
+	products, err := p.productUseCase.GetProductsUseCase()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, products)
+
+}
+
+func (p *ProductController) GetProductByCodeController(ctx *gin.Context) {
+
+	code := ctx.Param("codeProduct")
+
+	if code == "" {
+		response := model.Response{
+			Message: "Code not a nullable.",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	product, err := p.productUseCase.GetProductByCodeUseCase(code)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	if product == nil {
+		response := model.Response{
+			Message: "Product not found in data base.",
+		}
+		ctx.JSON(http.StatusNotFound, response)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, product)
+
 }
