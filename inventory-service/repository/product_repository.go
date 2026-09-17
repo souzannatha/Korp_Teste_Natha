33e02d58c58f27b84b18c8e0af54de86ce9dc848
souzannatha <souzanatha04@gmail.com>
2026-09-17 14:22:31 -0300

    feat: add  GetProductsRepository e GetProductByCodeRepository

--- inventory-service/repository/product_repository.go
+++ inventory-service/repository/product_repository.go
@@ -42,3 +42,66 @@ func (pr *ProductRepository) CreateProductRepository(product model.Product) (int
 
 	return id, nil
 }
+
+func (pr *ProductRepository) GetProductsRepository() ([]model.Product, error) {
+	query := "SELECT id, code, description,balance FROM products"
+	rows, err := pr.connection.Query(query)
+	if err != nil {
+		fmt.Println(err)
+		return []model.Product{}, err
+	}
+
+	var productList []model.Product
+	var productObj model.Product
+
+	for rows.Next() {
+		err = rows.Scan(
+			&productObj.ID,
+			&productObj.Code,
+			&productObj.Description,
+			&productObj.Balance)
+
+		if err != nil {
+			fmt.Println(err)
+			return []model.Product{}, err
+		}
+		productList = append(productList, productObj)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	return productList, nil
+}
+
+func (pr *ProductRepository) GetProductByCodeRepository(idProduct string) (*model.Product, error) {
+	query, err := pr.connection.Prepare(`SELECT id, code, description, balance
+	FROM products 
+	WHERE code = $1
+	`)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var product model.Product
+
+	err = query.QueryRow(idProduct).Scan(
+		&product.ID,
+		&product.Code,
+		&product.Description,
+		&product.Balance,
+	)
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	defer query.Close()
+	return &product, nil
+}
