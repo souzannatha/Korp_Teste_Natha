30d9549fa75e823cf4546e2386a1d72ee844d1c7
souzannatha <souzanatha04@gmail.com>
2026-09-17 19:08:29 -0300

    feat: add server in main.go

---
+++ billing-service/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	server := gin.Default()
+
+	server.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.Run(":8081")
+}
