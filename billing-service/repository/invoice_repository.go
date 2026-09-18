99f1a2a251627bdf8ffba205090761711b591164
souzannatha <souzanatha04@gmail.com>
2026-09-18 10:50:16 -0300

    feat: add func GetAllInvoicesRepository

--- billing-service/repository/invoice_repository.go
+++ billing-service/repository/invoice_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/souzannatha/Korp_Teste_Natha/billing-service/model"
 )
@@ -55,4 +56,72 @@ func (iv *InvoiceRepository) CreateInvoiceRepository(
 	}
 
 	return invoice, nil
+}
+
+func (iv *InvoiceRepository) GetAllInvoicesRepository() ([]model.Invoice, error) {
+	query := `
+	SELECT 
+	id, number, status 
+	FROM invoices 
+	ORDER BY number
+	`
+	rows, err := iv.connection.Query(query)
+	if err != nil {
+		fmt.Println(err)
+		return []model.Invoice{}, err
+	}
+	defer rows.Close()
+
+	invoices := []model.Invoice{}
+
+	for rows.Next() {
+		var invoice model.Invoice
+
+		err = rows.Scan(&invoice.ID, &invoice.Number, &invoice.Status)
+		if err != nil {
+			fmt.Println(err)
+			return []model.Invoice{}, err
+		}
+		invoices = append(invoices, invoice)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	for i := range invoices {
+		itemRows, err := iv.connection.Query(`
+		SELECT 
+		product_code, quantity 
+		FROM invoice_items 
+		WHERE invoice_id = $1
+		ORDER BY id
+	`, invoices[i].ID)
+
+		if err != nil {
+			return nil, err
+		}
+
+		invoices[i].Items = []model.InvoiceItem{}
+
+		for itemRows.Next() {
+			var item model.InvoiceItem
+
+			if err := itemRows.Scan(&item.ProductCode, &item.Quantity); err != nil {
+				itemRows.Close()
+				return nil, err
+			}
+
+			invoices[i].Items = append(invoices[i].Items, item)
+		}
+
+		if err := itemRows.Err(); err != nil {
+			itemRows.Close()
+			return nil, err
+		}
+
+		itemRows.Close()
+	}
+
+	rows.Close()
+	return invoices, nil
 }
